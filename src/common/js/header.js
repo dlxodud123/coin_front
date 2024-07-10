90a7f86dccf55fb2ee144480601a7f4f66b2a05a
taeyoung <xodud5080@naver.com>
2024-07-10 17:07:58 +0900

    시작

---
+++ src/common/js/header.js
@@ -0,0 +1,13 @@
+import './../css/header.css';
+
+const Header = () => {
+    return(
+        <>
+            <div className='header_container'>
+
+            </div>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/main/js/main_form.js
@@ -0,0 +1,10 @@
+import Header from "../../common/js/header";
+
+const Main_form = () => {
+    return(
+        <>
+            <Header></Header>
+        </>
+    )
+}
+export default Main_form;

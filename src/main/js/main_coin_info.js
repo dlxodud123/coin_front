e48ae89bc9a0df6ebc89370035731b86577410d1
taeyoung <xodud5080@naver.com>
2024-07-12 17:16:52 +0900

    2

--- src/main/js/main_coin_info.js
+++ src/main/js/main_coin_info.js
@@ -1,6 +1,5 @@
 import './../css/main_coin_info.css';
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const fetchTickerData = async () => {
@@ -16,25 +15,84 @@ const fetchTickerData = async () => {
 };
 
 const Main_coin_info = () => {
-  const { data, error, isLoading } = useQuery({
-    queryKey: 'tickerData',
-    queryFn: fetchTickerData,
-    // refetchInterval: 1000, // 5초마다 재호출
-  });
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  // const [formatPrice, setFormatPrice] = useState();
+  
+  const format_price = (price) => {
+    let formatPrice = price.toLacaleString('en-US');
+    return formatPrice;
+  }
 
-  if (isLoading) return <div>Loading...</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetchTickerData();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData(); // Initial fetch
+
+    const intervalId = setInterval(fetchData, 1000); // Fetch data every 1 second
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+
+  }, []);
+
   if (error) return <div>An error occurred: {error.message}</div>;
-  console.log("data",data)
+
+  // console.log(data[0].signed_change_rate);
 
   return (
+    data.length > 0 ?(
     <div className="main_coin_info_content">
-      코인 시세<br></br>
-      {data[0].opening_price}<br></br>
+      <div style={{fontSize:"35px", marginTop:"20px", marginLeft:"20px", height:"65px"}}>
+        코인 시세
+      </div>
+      <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+        <div className="main_coin_name">비트코인</div>
+        <div className='main_coin_price'>{data[0].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+        <div className='main_coin_rate'>{(data[0].signed_change_rate*100).toFixed(2)}%</div>
+      </div>
+      <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+      <div style={{display:"flex", marginLeft:"20px"}}>
+        <div className="main_coin_name">이더리움</div>
+        <div className='main_coin_price'>{data[1].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+        <div className='main_coin_rate'>{(data[1].signed_change_rate*100).toFixed(2)}%</div>
+      </div>
+      <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+      <div style={{display:"flex", marginLeft:"20px"}}>
+        <div className="main_coin_name">비트코인캐시</div>
+        <div className='main_coin_price'>{data[2].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+        <div className='main_coin_rate'>{(data[2].signed_change_rate*100).toFixed(2)}%</div>
+      </div>
+      <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+      <div style={{display:"flex", marginLeft:"20px"}}>
+        <div className="main_coin_name">솔라나</div>
+        <div className='main_coin_price'>{data[3].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+        <div className='main_coin_rate'>{(data[3].signed_change_rate*100).toFixed(2)}%</div>
+      </div>
+      <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+      <div style={{display:"flex", marginLeft:"20px"}}>
+        <div className="main_coin_name">에이브</div>
+        <div className='main_coin_price'>{data[4].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+        <div className='main_coin_rate'>{(data[4].signed_change_rate*100).toFixed(2)}%</div>
+      </div>
+      {/* {data[0].opening_price}<br></br>
       {data[1].opening_price}<br></br>
       {data[2].opening_price}<br></br>
       {data[3].opening_price}<br></br>
-      {data[4].opening_price}
+      {data[4].opening_price} */}
     </div>
+  ) : (
+    <div>asdf</div>
+  )
   );
 };
 

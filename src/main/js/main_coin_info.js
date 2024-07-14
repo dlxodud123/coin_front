3dc811ec94a1bce11341d0ba1ee7a393a2e648f8
taeyoung <xodud5080@naver.com>
2024-07-14 16:53:19 +0900

    4

--- src/main/js/main_coin_info.js
+++ src/main/js/main_coin_info.js
@@ -18,7 +18,11 @@ const Main_coin_info = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  // const [formatPrice, setFormatPrice] = useState();
+  const [coinName, setCoinName] = useState(['비트코인', '이더리움', '비트코인캐시', '솔라나', '에이브'])
+
+  const RISE = 'RISE';
+  const EVEN = 'EVEN';
+  const FALL = 'FALL';
   
   const format_price = (price) => {
     let formatPrice = price.toLacaleString('en-US');
@@ -47,20 +51,51 @@ const Main_coin_info = () => {
 
   if (error) return <div>An error occurred: {error.message}</div>;
 
-  // console.log(data[0].signed_change_rate);
-
   return (
     data.length > 0 ?(
     <div className="main_coin_info_content">
       <div style={{fontSize:"35px", marginTop:"20px", marginLeft:"20px", height:"65px"}}>
         코인 시세
       </div>
-      <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
-        <div className="main_coin_name">비트코인</div>
-        <div className='main_coin_price'>{data[0].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
-        <div className='main_coin_rate'>{(data[0].signed_change_rate*100).toFixed(2)}%</div>
-      </div>
-      <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+      {  data.map((coin, index) => {
+          console.log(index)
+          if (coin.change === RISE) {
+            return (
+              <>
+                <div key={index} style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <div className="main_coin_name">{coinName[index]}</div>
+                  <div style={{color:"red"}} className='main_coin_price'>{coin.trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+                  <div style={{color:"red"}} className='main_coin_rate'>+{(coin.signed_change_rate*100).toFixed(2)}%</div>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+              </>
+            );
+          } else if(coin.change === EVEN) {
+            return (
+              <>
+                <div key={index} style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <div className="main_coin_name">{coinName[index]}</div>
+                  <div className='main_coin_price'>{coin.trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+                  <div className='main_coin_rate'>{(coin.signed_change_rate*100).toFixed(2)}%</div>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+              </>
+            );
+          } else{
+            return (
+              <>
+                <div key={index} style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <div className="main_coin_name">{coinName[index]}</div>
+                  <div style={{color:"blue"}} className='main_coin_price'>{coin.trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
+                  <div style={{color:"blue"}} className='main_coin_rate'>-{(coin.signed_change_rate*100).toFixed(2)}%</div>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+              </>
+            );
+          }
+      })}
+
+      {/* <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
       <div style={{display:"flex", marginLeft:"20px"}}>
         <div className="main_coin_name">이더리움</div>
         <div className='main_coin_price'>{data[1].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
@@ -83,15 +118,10 @@ const Main_coin_info = () => {
         <div className="main_coin_name">에이브</div>
         <div className='main_coin_price'>{data[4].trade_price.toLocaleString('en-US')}&nbsp;KRW</div>
         <div className='main_coin_rate'>{(data[4].signed_change_rate*100).toFixed(2)}%</div>
-      </div>
-      {/* {data[0].opening_price}<br></br>
-      {data[1].opening_price}<br></br>
-      {data[2].opening_price}<br></br>
-      {data[3].opening_price}<br></br>
-      {data[4].opening_price} */}
+      </div> */}
     </div>
   ) : (
-    <div>asdf</div>
+    <div></div>
   )
   );
 };
--- src/main/js/main_news_info.js
+++ src/main/js/main_news_info.js
@@ -4,7 +4,39 @@ const Main_news_info = () => {
     return(
         <>
             <div className="main_coin_news_content">
-                asdf
+                <div style={{fontSize:"35px", marginTop:"20px", marginLeft:"20px", height:"65px"}}>
+                    코인 뉴스
+                </div>
+                <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <a style={{textDecoration:"none"}} href='https://www.dt.co.kr/contents.html?article_no=2024071402109963046004&ref=naver' className="main_coin_news">
+                    트럼프 피격에…<label style={{fontWeight:"bold"}}>비트코인</label> 6만달러 넘어섰다
+                  </a>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+                <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <a style={{textDecoration:"none"}} href='https://weekly.chosun.com/news/articleView.html?idxno=35918' className="main_coin_news">
+                    "2억 간다더니 여름 약세장 코앞?".... <label style={{fontWeight:"bold"}}>비트코인</label> 이렇게 끝나나
+                </a>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+                <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <a style={{textDecoration:"none"}} href='https://zdnet.co.kr/view/?no=20240714093223' className="main_coin_news">
+                    美 <label style={{fontWeight:"bold"}}>비트코인</label> ETF, 6일간 10억 달러 유입
+                  </a>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+                <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <a style={{textDecoration:"none"}} href='https://coinreaders.com/117215' className="main_coin_news">
+                    안정 되찾은 <label style={{fontWeight:"bold"}}>비트코인</label>...6만달러 저항 돌파 언제쯤
+                  </a>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
+                <div style={{display:"flex", marginLeft:"20px", alignItems:"center"}}>
+                  <a style={{textDecoration:"none"}} href='https://weekly.chosun.com/news/articleView.html?idxno=35929' className="main_coin_news">
+                    “<label style={{fontWeight:"bold"}}>비트코인</label>, 8월에 오른다” 8100만원 후퇴에도 실망 이르다?
+                  </a>
+                </div>
+                <div style={{height:"1px", width:"590px", backgroundColor:"rgba(0,0,0,0.1)", marginLeft:"20px"}}></div>
             </div>
         </>
     )
